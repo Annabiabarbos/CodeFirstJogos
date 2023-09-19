1f2af7a1a08c4b5e750d2b5e5dc8fff3b0fbd964
Anna Beatriz Barbosa Cheliga Horta <52161105850@info.local>
2023-09-19 08:43:34 -0300

    Atividade Inlock Jogos

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using WebCodeFirst.Intefaces;
+using WebCodeFirst.Repositories;
+using WebCodeFirst.ViewModels;
+
+namespace WebCodeFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        public LoginController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel usuario)
+        {
+
+        }
+    }
+}
---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebCodeFirst.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email obrigatorio!")]
+        public string? email { get; set; }
+
+        [Required(ErrorMessage ="Senha obrigatoria!")]
+        public string? senha { get; set; }
+    }
+}
